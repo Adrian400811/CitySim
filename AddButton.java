2c1c8c43f1beb3a5313967594b5598154787a135
349928465 <349928465@gapps.yrdsb.ca>
2024-04-07 18:37:15 -0400

    Added Constructor and Parameters for both Button subclasses

--- AddButton.java
+++ AddButton.java
@@ -12,6 +12,18 @@ public class AddButton extends Button
     private GreenfootImage unclickedButton = new GreenfootImage("Adjust_Up_Button_01.png");
     private GreenfootImage clickedButton = new GreenfootImage("Adjust_Up_Button_02.png");
     
+    public AddButton(String type){
+        if (type.equals("SI")){
+            
+        } else if (type.equals("EPR")){
+            
+        } else if (type.equals("WBI")){
+            
+        }
+        
+    }
+    
+    
     /**
      * Act - do whatever the AddButton wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
