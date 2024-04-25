1a6ed6fcda95cf551b54e719728d388f2f242ae5
Adrian400811 <adrianlee@adrian40.xyz>
2024-04-24 22:31:31 -0400

    Fixed syntax error

--- Settings.java
+++ Settings.java
@@ -116,7 +116,7 @@ public class Settings extends World {
   public void spawnSettingButton(){
       addObject(eventButton, getWidth()/4-150, getHeight()-180);
       addObject(roundButton, getWidth()/4+80, getHeight()-180);
-
+  }
   /** Checks event button */
   public void cheakEventButton() {
     if (eventButton.checkClicked()) {
